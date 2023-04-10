c9a84bed9b2c85c077ff31da06962611b30ae71b
Alexandr Lecomcev <opera9876@rambler.ru>
2023-04-10 22:27:18 +0300

    add: init files for trees test

---
+++ app/src/test/kotlin/trees/AVLTreeTest.kt
@@ -0,0 +1,20 @@
+package trees
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class AVLTreeTest {
+
+    @Test
+    fun balance() {
+    }
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun remove() {
+    }
+}
---
+++ app/src/test/kotlin/trees/RBTreeTest.kt
@@ -0,0 +1,20 @@
+package trees
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class RBTreeTest {
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun remove() {
+    }
+
+    @Test
+    fun balance() {
+    }
+}
