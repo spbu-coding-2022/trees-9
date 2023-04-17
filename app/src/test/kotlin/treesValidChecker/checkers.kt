554fda986698d5ae8a3c7b96820d8f2efcde537a
Alexandr Lecomcev <111463436+vacmannnn@users.noreply.github.com>
2023-04-18 02:13:58 +0300

    feat: find, add, remove tests for AVL

--- app/src/test/kotlin/treesValidChecker/checkers.kt
+++ app/src/test/kotlin/treesValidChecker/checkers.kt
@@ -1,7 +1,10 @@
 package treesValidChecker
 
+import trees.AVLTree
 import trees.BSTree
+import trees.nodes.AVLNode
 import trees.nodes.BSNode
+import kotlin.math.abs
 
 val BStree = BSTree<Int, Int>()
 
@@ -16,4 +19,15 @@ fun isBstOk(node: BSNode<Int, Int>? = BStree.root): Boolean {
         return false
     }
     return !(!isBstOk(node.right) || !isBstOk(node.left))
+}
+
+val AVLtree = AVLTree<Int, Int>()
+
+fun isAVLOk(node: AVLNode<Int, Int>? = AVLtree.root): Boolean {
+    if (node == null) {
+        return true
+    }
+    val lh = AVLtree.getHeight(node.left)
+    val rh = AVLtree.getHeight(node.right)
+    return abs(lh-rh) <= 1 && isAVLOk(node.left) && isAVLOk(node.right)
 }
