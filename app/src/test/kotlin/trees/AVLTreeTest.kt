ac6e8ddb525c11f11eacd66ef9958c7ef3fef33e
Alexandr Lecomcev <opera9876@rambler.ru>
2023-04-18 02:08:15 +0300

    feat: find, add, remove tests

--- app/src/test/kotlin/trees/AVLTreeTest.kt
+++ app/src/test/kotlin/trees/AVLTreeTest.kt
@@ -1,20 +1,44 @@
 package trees
 
 import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.Assertions.*
+import treesValidChecker.isAVLOk
 
 class AVLTreeTest {
+    private val tree = AVLTree<Int, Int>()
 
     @Test
-    fun balance() {
+    fun `find existing node`() {
+        for (i in (0..10000).shuffled()) {
+            tree.add(i, 0)
+        }
+        assertNotNull(tree.find(408))
     }
 
     @Test
-    fun add() {
+    fun `find not existing node`() {
+        for (i in (0..10000).shuffled()) {
+            tree.add(i, 0)
+        }
+        assertNull(tree.find(10001))
     }
 
     @Test
-    fun remove() {
+    fun `add 10000 elements`() {
+        for (i in (0..10000).shuffled()) {
+            tree.add(i, 0)
+            assertTrue(isAVLOk())
+        }
+    }
+
+    @Test
+    fun `remove 10000 elements`() {
+        for (i in (0..10000).shuffled()) {
+            tree.add(i, 0)
+        }
+        for (i in (0..10000).shuffled()) {
+            tree.remove(i)
+            assertTrue(isAVLOk())
+        }
     }
 }
