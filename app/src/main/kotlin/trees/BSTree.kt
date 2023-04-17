281a7d7f22027ee2e2aac7e5126fdf2eb0ea16e0
raf-nr <nurmuhametovrafik04@gmail.com>
2023-04-17 23:21:43 +0300

    fix: add and remove function in BST, add by key/value

--- app/src/main/kotlin/trees/BSTree.kt
+++ app/src/main/kotlin/trees/BSTree.kt
@@ -18,102 +18,40 @@ class BSTree<K : Comparable<K>, V> : BinaryTree<K, V, BSNode<K, V>>() {
         return !(!isBstOk(node.right) || !isBstOk(node.left))
     }
 
-    override fun add(node: BSNode<K, V>) {
-        if (root == null) {
-            root = node
-            return
+    override fun add(key: K, value: V) {
+        root = recursiveAdd(root, BSNode(key, value))
+    }
+
+    private fun recursiveAdd(currentNode: BSNode<K, V>?, node: BSNode<K, V>): BSNode<K, V> {
+        if (currentNode == null) return node
+        if (currentNode.key < node.key) {
+            currentNode.right = recursiveAdd(currentNode.right, node)
         }
-        var current = root
-        var parent = current
-        while (current != null) {
-            parent = current
-            current = when {
-                current.key < node.key -> current.right
-                current.key > node.key -> current.left
-                else -> throw IllegalArgumentException("Node with key ${node.key} is already in the tree")
-            }
+        if (currentNode.key > node.key) {
+            currentNode.left = recursiveAdd(currentNode.left, node)
         }
-        if (parent!!.key < node.key) {
-            parent.right = node
-        } else {
-            parent.left = node
-        }
+        return currentNode
     }
 
     override fun remove(key: K) {
         val node = find(key)
-        root?.let {
-            node?.let { itNode ->
-                if (itNode.key > it.key) scan(itNode, it.right, it)
-                if (itNode.key < it.key) scan(itNode, it.left, it)
-                else removeNode(it, null)
-            }
+        root = node?.let { recursiveRemove(root, it) }
+    }
+
+    private fun recursiveRemove(currentNode: BSNode<K, V>?, node: BSNode<K, V>): BSNode<K, V>? {
+        if (currentNode == null) return null
+        if (currentNode.key < node.key) {
+            currentNode.right = recursiveRemove(currentNode.right, node)
+        } else if (currentNode.key > node.key) {
+            currentNode.left = recursiveRemove(currentNode.left, node)
+        } else {
+            if (currentNode.left == null || currentNode.right == null)
+                return currentNode.left ?: currentNode.right
+            val minRightSubtree = getMinNode(currentNode.right!!)
+            currentNode.key = minRightSubtree.key
+            currentNode.value = minRightSubtree.value
+            currentNode.right = recursiveRemove(currentNode.right, minRightSubtree)
         }
-    }
-
-    private fun scan(node: BSNode<K, V>, current: BSNode<K, V>?, parent: BSNode<K, V>) {
-        if (current == null) {
-            println("value ${node.key} not exist in the tree")
-            return
-        }
-        when {
-            node.key > current.key -> scan(node, current.right, current)
-            node.key < current.key -> scan(node, current.left, current)
-            else -> removeNode(current, parent)
-        }
-    }
-
-    private fun removeNode(node: BSNode<K, V>, parent: BSNode<K, V>?) {
-        node.left?.let { leftChild ->
-            run {
-                node.right?.let {
-                    removeTwoChildNode(node)
-                } ?: removeSingleChildNode(node, leftChild)
-            }
-        } ?: run {
-            node.right?.let { rightChild -> removeSingleChildNode(node, rightChild) } ?: removeNoChildNode(node, parent)
-        }
-    }
-
-    private fun removeNoChildNode(node: BSNode<K, V>, parent: BSNode<K, V>?) {
-        parent?.let { p ->
-            if (node == p.left) {
-                p.left = null
-            } else if (node == p.right) {
-                p.right = null
-            }
-        } ?: rootRemove()
-    }
-
-    private fun rootRemove() {
-        root = null
-    }
-
-    private fun removeTwoChildNode(node: BSNode<K, V>) {
-        val leftChild = node.left
-        if (leftChild != null) {
-            leftChild.right?.let {
-                val maxParent = findParentOfMaxChild(leftChild)
-                maxParent.right?.let {
-                    node.key = it.key
-                    maxParent.right = null
-                } ?: throw IllegalStateException("Node with max child must have the right child!")
-
-            } ?: run {
-                node.key = leftChild.key
-                node.left = leftChild.left
-            }
-        }
-    }
-
-    private fun findParentOfMaxChild(n: BSNode<K, V>): BSNode<K, V> {
-        return n.right?.let { r -> r.right?.let { findParentOfMaxChild(r) } ?: n }
-            ?: throw IllegalArgumentException("Right child must be non-null")
-    }
-
-    private fun removeSingleChildNode(parent: BSNode<K, V>, child: BSNode<K, V>) {
-        parent.key = child.key
-        parent.left = child.left
-        parent.right = child.right
+        return currentNode
     }
 }
