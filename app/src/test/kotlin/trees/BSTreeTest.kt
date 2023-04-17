882b71e16462e1018bf52324af374c02dc353c52
Alexandr Lecomcev <opera9876@rambler.ru>
2023-04-17 22:40:58 +0300

    feat: tests for add and remove functions

--- app/src/test/kotlin/trees/BSTreeTest.kt
+++ app/src/test/kotlin/trees/BSTreeTest.kt
@@ -1,44 +1,44 @@
 package trees
 
 import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.TestInstance
 import trees.nodes.BSNode
-import kotlin.random.Random
 
-const val seed = 74
-
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class BSTreeTest {
-
-    private val newInt = Random(seed)
     private val tree = BSTree<Int, Int>()
-    // create pairs with int types, each int less than 'until' border
-    private val treePairs = Array(1000) { Pair(newInt.nextInt(10000), newInt.nextInt(10000)) }
-    @BeforeAll
-    // another way - make it @beforeEach and put away @TestInstance
-    fun init() {
-        for (i in 0 until 1000)
-            tree.add(BSNode(treePairs[i].first, treePairs[i].second))
-    }
 
     @Test
     fun `find existing node`() {
-        assertNotNull(tree.find(treePairs[408].first))
+        for (i in (0..10000).shuffled()) {
+            tree.add(BSNode(i, 0))
+        }
+        assertNotNull(tree.find(408))
     }
 
     @Test
     fun `find not existing node`() {
+        for (i in (0..10000).shuffled()) {
+            tree.add(BSNode(i, 0))
+        }
         assertNull(tree.find(10001))
     }
 
     @Test
-    fun add() {
+    fun `add 10000 elements`() {
+        for (i in (0..10000).shuffled()) {
+            tree.add(BSNode(i, 0))
+            assertTrue(tree.isBstOk())
+        }
     }
 
     @Test
-    fun remove() {
+    fun `remove 10000 elements`() {
+        for (i in (0..10000).shuffled()) {
+            tree.add(BSNode(i, 0))
+        }
+        for (i in (0..10000).shuffled()) {
+            tree.remove(i)
+            assertTrue(tree.isBstOk())
+        }
     }
 }
