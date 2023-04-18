6144101d9ea6eb9236c92cbad0b76cb6ac45dd34
Alexandr Lecomcev <opera9876@rambler.ru>
2023-04-19 01:30:39 +0300

    feat: implemented open and close functions, template for clean and export

--- app/src/main/kotlin/trees/dataBases/RBT/Neo4jRepository.kt
+++ app/src/main/kotlin/trees/dataBases/RBT/Neo4jRepository.kt
@@ -1,9 +1,41 @@
 package trees.dataBases.RBT
 
+import org.neo4j.driver.AuthTokens
+import org.neo4j.driver.Driver
+import org.neo4j.driver.GraphDatabase
+import org.neo4j.driver.TransactionContext
+import org.neo4j.driver.exceptions.SessionExpiredException
+import trees.nodes.RBNode
 import java.io.Closeable
+import java.io.IOException
 
 class Neo4jRepository: Closeable {
+    private var driver: Driver? = null
+
+    private fun cleanDataBase(tx: TransactionContext) {
+        TODO("Implement cleaning of DB")
+    }
+
+    fun exportRBTree(root: RBNode<Int, String>) {   // when we have treeView, fun will be rewritten
+        val session = driver?.session() ?: throw IOException("Driver is not open")
+        session.executeWrite { tx ->
+            TODO("Implement write to DB")
+        }
+        session.close()
+    }
+
+    fun open(uri: String, username: String, password: String) {
+        try {
+            driver = GraphDatabase.driver(uri, AuthTokens.basic(username, password))
+
+        } catch (ex: IllegalArgumentException) {
+            throw IOException("Wrong URI", ex)
+        } catch (ex: SessionExpiredException) {
+            throw IOException("Session failed, try restarting the app", ex)
+        }
+    }
+
     override fun close() {
-        TODO("Not yet implemented")
+        driver?.close()
     }
 }
