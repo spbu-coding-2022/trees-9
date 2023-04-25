3ac89e69dbbce877fc9be9dad56d33b380ebab25
Alexandr Lecomcev <opera9876@rambler.ru>
2023-04-25 06:15:40 +0300

    feat: HomeScreen buttons are the same size

--- app/src/main/kotlin/trees/App.kt
+++ app/src/main/kotlin/trees/App.kt
@@ -4,12 +4,15 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
@@ -18,6 +21,8 @@ import kotlin.system.exitProcess
 @Preview
 @Composable
 fun HomeScreen() {
+    val fontsize = 22
+    val buttonWidth = 225
     Column(Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,) {
@@ -25,25 +30,37 @@ fun HomeScreen() {
             onClick = {
                 TODO("TreeScreen")
             }) {
-            Text(text = "Binary search tree")
+            Text(text = "Binary search tree",
+                modifier = Modifier.width(buttonWidth.dp),
+                fontSize = fontsize.sp,
+                textAlign = TextAlign.Center)
         }
         Button(
             onClick = {
                 TODO("TreeScreen")
             }) {
-            Text(text = "AVL tree")
+            Text(text = "AVL tree",
+                modifier = Modifier.width(buttonWidth.dp),
+                fontSize = fontsize.sp,
+                textAlign = TextAlign.Center)
         }
         Button(
             onClick = {
                 TODO("TreeScreen")
             }) {
-            Text(text = "Red-black tree")
+            Text(text = "Red-black tree",
+                modifier = Modifier.width(buttonWidth.dp),
+                fontSize = fontsize.sp,
+                textAlign = TextAlign.Center)
         }
         Button(
             onClick = {
                 exitProcess(0)
             }) {
-            Text(text = "Exit")
+            Text(text = "Exit",
+                modifier = Modifier.width(buttonWidth.dp),
+                fontSize = fontsize.sp,
+                textAlign = TextAlign.Center)
         }
     }
 }
@@ -57,16 +74,3 @@ fun main() = application {
             HomeScreen()
     }
 }
-
-//val bst = insertAllNodesToTree()
-//        var x = 1
-//        val stack = mutableListOf(bst.root?.key)
-//        while (stack.isNotEmpty()) {
-//            val current = stack.removeLast()?.let { current -> bst.find(current) }
-//            if (current?.left != null)
-//                current.left?.key.apply(stack::add)
-//            if (current?.right != null)
-//                current.right?.key.apply(stack::add)
-//            Text("Node $x - ${current?.key}, ${current?.value}\n")
-//            x++
-//        }
