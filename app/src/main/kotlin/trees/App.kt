3a7ff6c34c00a806d9a8a02d6d436688be1d9de5
raf-nr <nurmuhametovrafik04@gmail.com>
2023-04-30 11:18:09 +0300

    add: menu of functions for actions on trees, implementation of the remaining windows

--- app/src/main/kotlin/trees/App.kt
+++ app/src/main/kotlin/trees/App.kt
@@ -10,6 +10,8 @@ import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import trees.gui.navigation.Screen
 import trees.gui.screens.BSTScreen
+import trees.gui.screens.AVLScreen
+import trees.gui.screens.RBScreen
 import trees.gui.screens.homeScreen
 
 fun main() = application {
@@ -21,10 +23,16 @@ fun main() = application {
         var screenState by remember { mutableStateOf<Screen>(Screen.homeScreen) }
         when (val screen = screenState) {
             is Screen.homeScreen ->
-                homeScreen(toBST = { screenState = Screen.BSTScreen })
+                homeScreen(toBST = { screenState = Screen.BSTScreen }, toAVL = { screenState = Screen.AVLScreen }, toRB = { screenState = Screen.RBScreen })
 
             is Screen.BSTScreen ->
                 BSTScreen(toMenu = { screenState = Screen.homeScreen})
+
+            is Screen.AVLScreen ->
+                AVLScreen(toMenu = { screenState = Screen.homeScreen})
+
+            is Screen.RBScreen ->
+                RBScreen(toMenu = { screenState = Screen.homeScreen})
         }
     }
 }   
