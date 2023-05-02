a32caf4bd99a133aa389e4ee811d35390db96ce6
Alexandr Lecomcev <opera9876@rambler.ru>
2023-05-02 08:04:02 +0300

    feat(BSTScreen): implemented add, remove, find functions

--- app/src/main/kotlin/trees/gui/screens/BSTScreen.kt
+++ app/src/main/kotlin/trees/gui/screens/BSTScreen.kt
@@ -1,19 +1,17 @@
 package trees.gui.screens
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -21,125 +19,206 @@ import trees.BSTree
 import trees.dataBases.BST.insertAllNodesToTree
 import trees.dataBases.BST.removeFile
 import trees.dataBases.BST.writeAllNodesToFile
-import trees.gui.printNode
+import trees.gui.*
 import trees.nodes.BSNode
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layout
-import androidx.compose.ui.modifier.modifierLocalMapOf
+
+var treeInit = false
+var findMode = false
+var enteredValue: String = ""
+var enteredKey: String = ""
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun BSTScreen(toMenu: () -> Unit) {
-    val tree = insertAllNodesToTree()
-//    val tree = BSTree<Int, String>()
-//    tree.root = BSNode(123, "717.875;217.375;.25;dsfsafds")
-//    tree.root?.left = BSNode(75, "633.625;481.875;875;kkdks")
-//    tree.root?.right = BSNode(3459, "222.388;486.055;18;1asdf")
-    val node = tree.root
+fun BSTScreen(toMenu: () -> Unit, toBST: () -> Unit) {
+    val tree = remember { mutableStateOf(BSTree<Int, String>()) }
+    var screenReload by remember { mutableStateOf(false) }
+    if (screenReload) {
+        screenReload = false
+    }
+    if (!treeInit) {
+        tree.value = insertAllNodesToTree()
+        treeInit = !treeInit
+    }
+    val node = tree.value.root
     val stack = mutableListOf(node?.key)
     while (stack.isNotEmpty()) {
-        val current = stack.removeLast()?.let { current -> tree.find(current) }
+        val current = stack.removeLast()?.let { current -> tree.value.find(current) }
         if (current?.left != null)
             current.left?.key.apply(stack::add)
         if (current?.right != null)
             current.right?.key.apply(stack::add)
-        current?.let { printNode(it) }
+        current?.let {
+            if (findMode && (enteredKey.toIntOrNull() != null) && it.key == enteredKey.toInt() && (tree.value.find(
+                    enteredKey.toInt()
+                ) != null)
+            ) {
+                printNode(it, true)
+            } else {
+                printNode(it)
+            }
+            val parent = tree.value.root?.let { it1 -> getParent(it.key, it1) }
+            parent?.let { it1 ->
+                if (tree.value.root != it) {
+                    printLine(it1, it)
+                }
+            }
+        }
     }
-    Box() {
+    Box {
         Column(
             Modifier.padding(10.dp, 10.dp, 0.dp, 0.dp).background(Color.Unspecified).shadow(1.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start,
         ) {
-            Column(
-                Modifier.width(200.dp).height(260.dp).offset(0.dp, 0.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
+            Row(
+                verticalAlignment = Alignment.Top
             ) {
-                var key by remember { mutableStateOf("") }
-                TextField(
-                    modifier = Modifier.width(180.dp).padding(vertical = 15.dp).clip(RoundedCornerShape(25.dp)),
-                    value = key,
-                    onValueChange = { key = it },
-                    label = { Text("Enter key") }
-                )
-                var value by remember { mutableStateOf("") }
-                TextField(
-                    modifier = Modifier.width(180.dp).padding(bottom = 15.dp).clip(RoundedCornerShape(25.dp)),
-                    value = value,
-                    onValueChange = { value = it },
-                    label = { Text("Enter value") }
-                )
-                Button(
-                    onClick = {
-                    },
+                Column(
+                    verticalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Text(
-                        text = "Insert node",
-                        modifier = Modifier.width(110.dp),
-                        fontSize = 16.sp,
-                        textAlign = TextAlign.Center
-                    )
+                    Column(
+                        Modifier.width(200.dp).height(150.dp).offset(0.dp, 0.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Button(
+                            onClick = {
+                                findMode = false
+                                var coordinate = Coordinate(0f,0f)
+                                var parent = BSNode(0,"")
+                                var value = settingValue(enteredValue, coordinate.x+50.0f, coordinate.y+.0f, 0, false)
+                                var curNode = BSNode(enteredKey.toInt(), value)
+                                tree.value.add(curNode.key, curNode.value)
+                                if (tree.value.root?.key != enteredKey.toInt()) {
+                                    parent = getParent(enteredKey.toInt(), tree.value.root!!)
+                                    coordinate = getCoordinate(parent.value)
+                                    println("${coordinate.x} ${coordinate.y}")
+                                }
+                                val point = findBracketPoint(value)
+                                val offset = 85f
+                                value = if (parent.left?.key == enteredKey.toInt()) {
+                                    settingValue(value, coordinate.x-offset, coordinate.y+offset, point, true)
+                                } else {
+                                    settingValue(value, coordinate.x+offset, coordinate.y+offset, point, true)
+                                }
+                                curNode = BSNode(enteredKey.toInt(), value)
+                                tree.value.add(curNode.key, curNode.value)
+                                screenReload = true
+                            },
+                        ) {
+                            Text(
+                                text = "Insert node",
+                                modifier = Modifier.width(110.dp),
+                                fontSize = 16.sp,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                        Button(
+                            onClick = {
+                                if (findMode) {
+                                    findMode = false
+                                }
+                                tree.value.remove(enteredKey.toInt())
+                                screenReload = true
+                            }) {
+                            Text(
+                                text = "Remove node",
+                                modifier = Modifier.width(110.dp),
+                                fontSize = 16.sp,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                        Button(
+                            onClick = {
+                                findMode = !findMode
+                                screenReload = true
+                            }) {
+                            Text(
+                                text = "Find mode",
+                                modifier = Modifier.width(110.dp),
+                                fontSize = 16.sp,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                    Column(
+                        Modifier.width(200.dp).height(150.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Button(
+                            onClick = {
+                                removeFile()
+                                writeAllNodesToFile(tree.value.root, tree.value)
+                            }) {
+                            Text(
+                                text = "Save tree",
+                                modifier = Modifier.width(110.dp),
+                                fontSize = 16.sp,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                        Button(
+                            onClick = {
+                                removeFile()
+                            }) {
+                            Text(
+                                text = "Clear DB file",
+                                modifier = Modifier.width(110.dp),
+                                fontSize = 16.sp,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                        Button(
+                            onClick = {
+                                treeInit = !treeInit
+                                toMenu()
+                            }) {
+                            Text(
+                                text = "Back to menu",
+                                modifier = Modifier.width(110.dp),
+                                fontSize = 16.sp,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
                 }
-                Button(
-                    onClick = {
-                    }) {
-                    Text(
-                        text = "Remove node",
-                        modifier = Modifier.width(110.dp),
-                        fontSize = 16.sp,
-                        textAlign = TextAlign.Center
-                    )
-                }
-                Button(
-                    onClick = {
-                    }) {
-                    Text(
-                        text = "Find node",
-                        modifier = Modifier.width(110.dp),
-                        fontSize = 16.sp,
-                        textAlign = TextAlign.Center
-                    )
-                }
-            }
-            Column(
-                Modifier.width(200.dp).height(180.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Button(
-                    onClick = {
-                        removeFile()
-                        writeAllNodesToFile(tree.root, tree)
-                    }) {
-                    Text(
-                        text = "Save tree",
-                        modifier = Modifier.width(110.dp),
-                        fontSize = 16.sp,
-                        textAlign = TextAlign.Center
-                    )
-                }
-                Button(
-                    onClick = {
-                        removeFile()
-                    }) {
-                    Text(
-                        text = "Clear DB file",
-                        modifier = Modifier.width(110.dp),
-                        fontSize = 16.sp,
-                        textAlign = TextAlign.Center
-                    )
-                }
-                Button(
-                    onClick = {
-                        toMenu()
-                    }) {
-                    Text(
-                        text = "Back to menu",
-                        modifier = Modifier.width(110.dp),
-                        fontSize = 16.sp,
-                        textAlign = TextAlign.Center
-                    )
+                Column() {
+                    Column(
+                        Modifier.width(200.dp).height(260.dp).offset(25.dp, 10.dp),
+                        verticalArrangement = Arrangement.Top,
+                        horizontalAlignment = Alignment.Start,
+                    ) {
+                        val fieldsWidth = 140
+                        var key by remember { mutableStateOf("") }
+                        OutlinedTextField(
+                            modifier = Modifier
+                                .width(fieldsWidth.dp)
+                                .height(65.dp)
+                                .padding(vertical = 5.dp),
+                            value = key,
+                            onValueChange = {
+                                key = it
+                                enteredKey = it
+                            },
+                            label = { Text("Enter key", textAlign = TextAlign.Center) }
+                        )
+                        var value by remember { mutableStateOf("") }
+                        OutlinedTextField(
+                            modifier = Modifier
+                                .width(fieldsWidth.dp)
+                                .height(65.dp)
+                                .padding(vertical = 5.dp),
+                            value = value,
+                            onValueChange = {
+                                value = it
+                                enteredValue = it
+                            },
+                            label = { Text("Enter value", textAlign = TextAlign.Center) }
+                        )
+                    }
+                    Text("all shit")
                 }
             }
         }
